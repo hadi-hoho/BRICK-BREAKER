1c9da2cc59f5c45d06d82c7f0a4259b4ee4dd8f8
sanaz motie <s.motie81@gmail.com>
2021-01-19 13:37:13 +0330

    print bricks error

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-// Created on A.R.M1111
+
 
 #include <iostream>
 //conio.h for getch and detection of pressing any key
 #include <conio.h>
 #include <unistd.h>
 
-#define maxx	30
+#define maxx	40
 #define maxy	30
 #define minx	0
 #define miny	0
@@ -18,21 +18,37 @@
 #define north_west 2
 #define south_east 3
 #define south_west 4
-using namespace std;
 
+#define bricks_length	3
+#define bricks_width	2
+#define bricks_number	9
+#define bricks_row1		5
+#define bricks_culomn1	10
+#define bricks_row2		10
+#define bricks_culomn2	20
+#define bricks_row3		15
+#define bricks_culomn3	30
+using namespace std;
+//struct of slider
 struct cordinate
 {
 	int x=slider_start_x;
 
 }slider;
 
+struct bricks
+{
+	int x;
+	int y;
+	bool visibility=true;
+}brick[bricks_number];
 //struct of ball
 
 struct ball
 {
     int heading=north_east; // 1= up-right 2=up-left 3=down-left 4=down-right
     bool visible = true;
-    int pos_x = maxx/2 -1; //index of posision of ball , in the middle bottom for defualt (higher than slider)
+    int pos_x = ((maxx/2) -1); //index of posision of ball , in the middle bottom for defualt (higher than slider)
     int pos_y = maxy-2; 
     char c = 'o';
 };
@@ -82,7 +98,17 @@ ball move_ball(ball target_ball,char **screen)
     return target_ball;
 }
 
-
+void print_bricks(char **screen)
+{
+	for(int i=0;(i<bricks_number) && (brick[i].visibility == true);i++)
+	{
+		for(int j=0;j<bricks_length;j++)
+		{
+			for(int k=0;k<bricks_width;k++)
+				screen[brick[i].y+k][brick[i].x+j]='#';
+		}
+	}
+}
 bool print_screen ()
 {
     //defing the array
@@ -115,6 +141,15 @@ bool print_screen ()
     {
     	screen[slider_y][j]='=';    	
 	}
+	//print_bricks(screen);
+	for(int i=0;(i<bricks_number) && (brick[i].visibility == true);i++)
+	{
+		for(int j=0;j<bricks_length;j++)
+		{
+			for(int k=0;k<bricks_width;k++)
+				screen[brick[i].y+k][brick[i].x+j]='#';
+		}
+	}
 	
     //printing the array
     for (int i = miny; i < maxy; i++)
@@ -130,6 +165,7 @@ bool print_screen ()
     	
     return true;
 }
+
 int slider_move(void)
 {
 	if(kbhit())
@@ -148,8 +184,32 @@ int slider_move(void)
 		}
 	return 0;
 }
+void initialize(void)
+{
+	brick[0].x=bricks_culomn1;
+	brick[0].y=bricks_row1;
+	brick[1].x=bricks_culomn2;
+	brick[1].y=bricks_row1;
+	brick[2].x=bricks_culomn3;
+	brick[2].y=bricks_row1;
+	
+	brick[3].x=bricks_culomn1;
+	brick[3].y=bricks_row2;
+	brick[4].x=bricks_culomn2;
+	brick[4].y=bricks_row2;
+	brick[5].x=bricks_culomn3;
+	brick[5].y=bricks_row2;
+	
+	brick[6].x=bricks_culomn1;
+	brick[6].y=bricks_row3;
+	brick[7].x=bricks_culomn2;
+	brick[7].y=bricks_row3;
+	brick[8].x=bricks_culomn3;
+	brick[8].y=bricks_row3;
+}
 int main()
 {
+	initialize();
 	while(1)
 	{
 		
