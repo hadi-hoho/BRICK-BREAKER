3fb469c0e1694262de562088f3e1b9fd41ef9ad9
hadi-hoho <haaadaia@gmail.com>
2021-01-19 08:57:28 +0330

    Update main.cpp
    added two funcs and a struct for the ball
    ball
    move_ball
    add_ball

--- main.cpp
+++ main.cpp
@@ -10,6 +10,59 @@
 #define miny	0
 using namespace std;
 
+//struct of ball
+
+struct ball
+{
+    int heading=1; // 1= up-right 2=up-left 3=down-left 4=down-right
+    bool visible = true;
+    int pos_x = maxx/2 -1; //index of posision of ball , in the middle bottom for defualt (higher than slider)
+    int pos_y = maxy-2; 
+    char c = 'o';
+};
+
+//add_ball adds a new ball to the screen
+ball add_ball (char **screen)
+{
+    ball new_ball;
+    screen[new_ball.pos_x][new_ball.pos_y] = new_ball.c;
+    return new_ball;
+}
+
+//move_ball moves the ball according to it's heading
+ball move_ball(ball target_ball,char **screen)
+{
+    if (target_ball.heading==1)
+    {
+        screen[target_ball.pos_x][target_ball.pos_y] = ' ';
+        screen[target_ball.pos_x+1][target_ball.pos_y-1] = target_ball.c;
+        target_ball.pos_x++;
+        target_ball.pos_y--;
+    }
+    if (target_ball.heading==2)
+    {
+        screen[target_ball.pos_x][target_ball.pos_y] = ' ';
+        screen[target_ball.pos_x-1][target_ball.pos_y-1] = target_ball.c;
+        target_ball.pos_x--;
+        target_ball.pos_y--;
+    }
+    if (target_ball.heading==3)
+    {
+        screen[target_ball.pos_x][target_ball.pos_y] = ' ';
+        screen[target_ball.pos_x-1][target_ball.pos_y+1] = target_ball.c;
+        target_ball.pos_x--;
+        target_ball.pos_y++;
+    }
+    if (target_ball.heading==4)
+    {
+        screen[target_ball.pos_x][target_ball.pos_y] = ' ';
+        screen[target_ball.pos_x+1][target_ball.pos_y+1] = target_ball.c;
+        target_ball.pos_x++;
+        target_ball.pos_y++;
+    }
+    return target_ball;
+}
+
 bool print_screen ()
 {
     //defing the array
