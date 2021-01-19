01218c8879f9c8c9f44c5d56886cb06b3e6ac3cc
sanaz motie <s.motie81@gmail.com>
2021-01-19 09:35:28 +0330

    slider move

--- main.cpp
+++ main.cpp
@@ -8,12 +8,22 @@
 #define maxy	50
 #define minx	0
 #define miny	0
+
+#define silder_length	10
+#define slider_start_y	20
+#define slider_x		27
 using namespace std;
 
+struct cordinate
+{
+	int y=slider_start_y;
+
+}slider;
 bool print_screen ()
 {
     //defing the array
     char screen[maxx][maxy];
+    
     for (int i = minx; i < maxx; i++)
     {
         if (i==(maxx-1) || i==minx)
@@ -33,48 +43,55 @@ bool print_screen ()
     }
     for (int i=minx;i<maxx;i++)
     {
-     	screen[i][0]='|';
-    	screen[i][49]='|';
+     	screen[i][miny]='|';
+    	screen[i][maxy-1]='|';
     }
-    screen[25][10]='i';
-    screen[25][11]='m';
-    screen[25][12]=' ';
-    screen[25][13]='h';
-    screen[25][14]='e';
-    screen[25][15]='r';
-    screen[25][16]='e';
-    screen[25][17]='!';
+    
+    for(int j= slider.y;j<(silder_length+slider.y);j++)
+    {
+    	screen[slider_x][j]='=';    	
+	}
+	
     //printing the array
     for (int i = minx; i < maxx; i++)
     {
+    	
         for (int j = miny; j < maxy; j++)
         {
             cout<<screen[i][j];
         }
         cout<<'\n';
     }
+    
+    	
     return true;
 }
-
+int slider_move(void)
+{
+	if(kbhit())
+		switch(getch())
+		{
+			case 'a':
+			case 'A':
+				slider.y--;
+				break;
+			case 'd':
+			case 'D':
+				slider.y++;
+				break;
+			default:
+				return 0;
+		}
+	return 0;
+}
 int main()
 {
-
-    print_screen();
-
-    char pressed_key ;
-    while (1)
-    {
-        pressed_key=getch();
-        if (pressed_key == 'd')
-        {
-            cout<<"right!"<<endl;
-        }
-        else if (pressed_key =='a')
-        {
-            cout<<"left!"<<endl;
-        }
-
-    }
+	while(1)
+	{
+		system("cls");
+		print_screen();
+		slider_move();	
+	}
     
     return 0;
 }
