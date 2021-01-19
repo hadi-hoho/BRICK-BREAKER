705566a8b931c8cc49fa8b68c8860f1462b8e061
hadi-hoho <haaadaia@gmail.com>
2021-01-19 13:29:03 +0330

    Ball added
    added a ball deleted the add_ball func
    ball moves

--- main.cpp
+++ main.cpp
@@ -14,6 +14,9 @@
 #define slider_start_x	20
 #define slider_y		28
 
+#define ball_start_x 14
+#define ball_start_y 27
+
 #define north_east 1
 #define north_west 2
 #define south_west 3
@@ -30,60 +33,48 @@ struct cordinate
 
 struct ball
 {
-    int heading=north_east; // 1= up-right 2=up-left 3=down-left 4=down-right
+    int heading = north_east;
     bool visible = true;
-    int pos_x = maxx/2 -1; //index of posision of ball , in the middle bottom for defualt (higher than slider)
-    int pos_y = maxy-2; 
+    int pos_x = ball_start_x; //index of posision of ball , in the middle bottom for defualt (higher than slider)
+    int pos_y = ball_start_y; 
     char c = 'o';
 };
 
-//add_ball adds a new ball to the screen
-ball add_ball (char **screen)
-{
-    ball new_ball;
-    screen[new_ball.pos_y][new_ball.pos_x] = new_ball.c;
-    return new_ball;
-}
 
 //move_ball moves the ball according to it's heading
-ball move_ball(ball target_ball,char **screen)
+void move_ball(ball *target_ball)
 {
-    switch (target_ball.heading)
+    switch (target_ball->heading)
     {
         case north_east: 
         {
-            screen[target_ball.pos_y][target_ball.pos_x] = ' ';
-            screen[target_ball.pos_y - 1][target_ball.pos_x + 1] = target_ball.c;
-            target_ball.pos_x++;
-            target_ball.pos_y--;
+            target_ball->pos_x ++ ;
+            target_ball->pos_y -- ;
+            break;
         }
         case north_west:
         {
-            screen[target_ball.pos_y][target_ball.pos_x] = ' ';
-            screen[target_ball.pos_y - 1][target_ball.pos_x - 1] = target_ball.c;
-            target_ball.pos_x--;
-            target_ball.pos_y--;
+            target_ball->pos_x -- ;
+            target_ball->pos_y -- ;
+            break;
         }
         case south_west:
         {
-            screen[target_ball.pos_y][target_ball.pos_x] = ' ';
-            screen[target_ball.pos_y + 1][target_ball.pos_x - 1] = target_ball.c;
-            target_ball.pos_x++;
-            target_ball.pos_y++;
+            target_ball->pos_x ++ ;
+            target_ball->pos_y ++ ;
+            break;
         }
         case south_east:
         {
-            screen[target_ball.pos_y][target_ball.pos_x] = ' ';
-            screen[target_ball.pos_y + 1][target_ball.pos_x + 1] = target_ball.c;
-            target_ball.pos_x--;
-            target_ball.pos_y++;
+            target_ball->pos_x -- ;
+            target_ball->pos_y ++ ;
+            break;
         }
     }
-    return target_ball;
 }
 
 
-bool print_screen ()
+bool print_screen (ball& target_ball)
 {
     //defing the array
     char screen[maxy][maxx];
@@ -110,11 +101,18 @@ bool print_screen ()
      	screen[i][minx]='|';
     	screen[i][maxx-1]='|';
     }
-    
+
+    //adding the slider
     for(int j= slider.x;j<(silder_length+slider.x);j++)
     {
-    	screen[slider_y][j]='=';    	
+    	screen[slider_y][j] = '=';    	
 	}
+
+    //adding the ball(s)
+     screen[target_ball.pos_y][target_ball.pos_x] = target_ball.c;
+    
+    
+
 	
     //printing the array
     for (int i = miny; i < maxy; i++)
@@ -150,14 +148,18 @@ int slider_move(void)
 }
 int main()
 {
+    
+
+    //adding a ball
+    ball ball_no_1;
+
 	while(1)
 	{
-		
-		print_screen();
+		print_screen(ball_no_1);
 		slider_move();
+        move_ball(&ball_no_1);
         usleep(100000);
         system("cls");
-        	
 	}
     return 0;
 }
