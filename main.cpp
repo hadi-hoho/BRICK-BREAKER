f95c69ea44e43e3b588f3ad7d22e6c2b339003c2
hadi-hoho <haaadaia@gmail.com>
2021-01-19 11:17:21 +0330

    x,y solved
    changed if to switch case in move_ball
    changed headings to defined objects
    
    switched x and y
    added a sleep timer

--- main.cpp
+++ main.cpp
@@ -3,29 +3,34 @@
 #include <iostream>
 //conio.h for getch and detection of pressing any key
 #include <conio.h>
+#include <unistd.h>
 
 #define maxx	30
-#define maxy	50
+#define maxy	30
 #define minx	0
 #define miny	0
 
 #define silder_length	10
-#define slider_start_y	20
-#define slider_x		27
+#define slider_start_x	20
+#define slider_y		28
+
+#define north_east 1
+#define north_west 2
+#define south_east 3
+#define south_west 4
 using namespace std;
 
-<<<<<<< HEAD
 struct cordinate
 {
-	int y=slider_start_y;
+	int x=slider_start_x;
 
 }slider;
-=======
+
 //struct of ball
 
 struct ball
 {
-    int heading=1; // 1= up-right 2=up-left 3=down-left 4=down-right
+    int heading=north_east; // 1= up-right 2=up-left 3=down-left 4=down-right
     bool visible = true;
     int pos_x = maxx/2 -1; //index of posision of ball , in the middle bottom for defualt (higher than slider)
     int pos_y = maxy-2; 
@@ -36,83 +41,86 @@ struct ball
 ball add_ball (char **screen)
 {
     ball new_ball;
-    screen[new_ball.pos_x][new_ball.pos_y] = new_ball.c;
+    screen[new_ball.pos_y][new_ball.pos_x] = new_ball.c;
     return new_ball;
 }
 
 //move_ball moves the ball according to it's heading
 ball move_ball(ball target_ball,char **screen)
 {
-    if (target_ball.heading==1)
+    switch (target_ball.heading)
     {
-        screen[target_ball.pos_x][target_ball.pos_y] = ' ';
-        screen[target_ball.pos_x+1][target_ball.pos_y-1] = target_ball.c;
-        target_ball.pos_x++;
-        target_ball.pos_y--;
-    }
-    if (target_ball.heading==2)
-    {
-        screen[target_ball.pos_x][target_ball.pos_y] = ' ';
-        screen[target_ball.pos_x-1][target_ball.pos_y-1] = target_ball.c;
-        target_ball.pos_x--;
-        target_ball.pos_y--;
-    }
-    if (target_ball.heading==3)
-    {
-        screen[target_ball.pos_x][target_ball.pos_y] = ' ';
-        screen[target_ball.pos_x-1][target_ball.pos_y+1] = target_ball.c;
-        target_ball.pos_x--;
-        target_ball.pos_y++;
-    }
-    if (target_ball.heading==4)
-    {
-        screen[target_ball.pos_x][target_ball.pos_y] = ' ';
-        screen[target_ball.pos_x+1][target_ball.pos_y+1] = target_ball.c;
-        target_ball.pos_x++;
-        target_ball.pos_y++;
+        case north_east: 
+        {
+            screen[target_ball.pos_y][target_ball.pos_x] = ' ';
+            screen[target_ball.pos_y - 1][target_ball.pos_x + 1] = target_ball.c;
+            target_ball.pos_x++;
+            target_ball.pos_y--;
+        }
+        case north_west:
+        {
+            screen[target_ball.pos_y][target_ball.pos_x] = ' ';
+            screen[target_ball.pos_y - 1][target_ball.pos_x - 1] = target_ball.c;
+            target_ball.pos_x--;
+            target_ball.pos_y--;
+        }
+        case south_west:
+        {
+            screen[target_ball.pos_y][target_ball.pos_x] = ' ';
+            screen[target_ball.pos_y + 1][target_ball.pos_x - 1] = target_ball.c;
+            target_ball.pos_x++;
+            target_ball.pos_y++;
+        }
+        case south_east:
+        {
+            screen[target_ball.pos_y][target_ball.pos_x] = ' ';
+            screen[target_ball.pos_y + 1][target_ball.pos_x + 1] = target_ball.c;
+            target_ball.pos_x--;
+            target_ball.pos_y++;
+        }
     }
     return target_ball;
 }
 
->>>>>>> 3fb469c0e1694262de562088f3e1b9fd41ef9ad9
+
 bool print_screen ()
 {
     //defing the array
-    char screen[maxx][maxy];
+    char screen[maxy][maxx];
     
-    for (int i = minx; i < maxx; i++)
+    for (int i = miny; i < maxy; i++)
     {
-        if (i==(maxx-1) || i==minx)
+        if (i==(maxy-1) || i==miny)
         {
-            for (int j = miny; j < maxy; j++)
+            for (int j = minx; j < maxx; j++)
             {
                 screen[i][j]= '_';
             }
         }
         else
 		{
-            for (int j = miny; j < maxy; j++)
+            for (int j = minx; j < maxx; j++)
             {
                 screen[i][j]= ' ';
             }
         }
     }
-    for (int i=minx;i<maxx;i++)
+    for (int i=miny;i<maxy;i++)
     {
-     	screen[i][miny]='|';
-    	screen[i][maxy-1]='|';
+     	screen[i][minx]='|';
+    	screen[i][maxx-1]='|';
     }
     
-    for(int j= slider.y;j<(silder_length+slider.y);j++)
+    for(int j= slider.x;j<(silder_length+slider.x);j++)
     {
-    	screen[slider_x][j]='=';    	
+    	screen[slider_y][j]='=';    	
 	}
 	
     //printing the array
-    for (int i = minx; i < maxx; i++)
+    for (int i = miny; i < maxy; i++)
     {
     	
-        for (int j = miny; j < maxy; j++)
+        for (int j = minx; j < maxx; j++)
         {
             cout<<screen[i][j];
         }
@@ -129,11 +137,11 @@ int slider_move(void)
 		{
 			case 'a':
 			case 'A':
-				slider.y--;
+				slider.x--;
 				break;
 			case 'd':
 			case 'D':
-				slider.y++;
+				slider.x++;
 				break;
 			default:
 				return 0;
@@ -144,10 +152,12 @@ int main()
 {
 	while(1)
 	{
-		system("cls");
+		
 		print_screen();
-		slider_move();	
+		slider_move();
+        usleep(100000);
+        system("cls");
+        	
 	}
-    
     return 0;
 }
